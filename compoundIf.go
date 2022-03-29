86eca0e4699a9884fc1a5824ab90580642e88611
williamneo <williamneokh@gmail.com>
2022-03-29 23:37:45 +0800

    Leap year checker done

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert your code here
+
+	var year int
+	for {
+		fmt.Println("Key in the year to check if it's a leap year")
+		_, _ = fmt.Scanln(&year)
+		checkYear(year)
+		fmt.Println("------------")
+	}
+
+}
+func checkYear(year int) {
+	if year%4 == 0 {
+		if year%100 != 0 {
+			fmt.Println(year, "is a leap year")
+		} else if year%100 == 0 {
+			if year%400 == 0 {
+				fmt.Println(year, "is a leap year")
+			} else {
+				fmt.Println(year, "is not a leap year")
+			}
+		}
+	} else {
+
+		fmt.Println(year, "is not a leap year")
+	}
 }
